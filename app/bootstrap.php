ff7369ab99b6d20650910681c821c2a675fbfd74
Ivan Matozan <ivan.matozan@gmail.com>
2017-08-06 11:30:03 +0200

    Initialze PDO

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,3 +3,9 @@
 // Composer autoload
 require __DIR__ . '/../vendor/autoload.php';
 
+try {
+    // PDO
+    $db = new PDO('mysql:host=localhost;dbname=todo', 'root', '');
+} catch (PDOException $e) {
+    die($e->getMessage());
+}
